910a736011276cafd2772c2d9bc779519e694d8a
BertrandBurel <legionxful@gmail.com>
2019-11-27 19:55:40 +0100

    advanced routing

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -10,12 +10,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("wild/index", name="wild_index")
      */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "wild/show/{slug}",
+     *     defaults={"slug"=""},
+     *     requirements={"slug"="[a-z0-9\-]+"},
+     *     name="wild_show"
+     * )
+     */
+    public function show(string $slug) : Response
+    {
+        if ($slug === "") {
+            $slug = "Aucune série sélectionnée, veuillez choisir une série.";
+        } else {
+            $slug = ucwords(str_replace("-", " ", $slug));
+        }
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug
         ]);
     }
 }
